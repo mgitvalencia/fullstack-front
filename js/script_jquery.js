5596df4ec25c14d35c6e8a5bf61e49589014c33c
mgitvalencia <105307230+mgitvalencia@users.noreply.github.com>
2022-06-04 18:24:42 -0500

    Disponibilidad de pizza

--- js/script_jquery.js
+++ js/script_jquery.js
@@ -6,11 +6,30 @@ $(function() {
         if($("#nombre").val() == "" || $("#apellido").val() == "" ) {
             if($("#nombre").val() == "") {
                 $("#lblnombre").css("color","red");                
+            }else{
+                $("#lblnombre").css("color","black");                
             };
             if($("#apellido").val() == "") {
                 $("#lblapellido").css("color","red");
+            }else{
+                $("#lblapellido").css("color","black");
             };            
             event.preventDefault();
-        };
+        }else{
+            $("#lblnombre").css("color","black");
+            $("#lblapellido").css("color","black");
+            };
     });
+});
+
+$("#tamano").on('change', function() {
+    $("#resultado_tamano").html("");
+    
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:5000/checksize",
+        data: { tamano: this.value  },
+        success: function(htmlexterno){
+           $("#resultado_tamano").html(htmlexterno);}
+    })
 });
